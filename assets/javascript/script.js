9081cf87fff055b5643f8fdd2f54ad8964c8c328
rukaroa <rukaroa@gmail.com>
2017-03-04 13:33:58 -0600

    initial files and initialization

---
+++ assets/javascript/script.js
@@ -0,0 +1,11 @@
+/ Initialize Firebase
+ var config = {
+   apiKey: "AIzaSyBFgdL37fvv-QG8B5iJyyXnCqo2rKM3Egs",
+   authDomain: "coding-bootcamp-project1.firebaseapp.com",
+   databaseURL: "https://coding-bootcamp-project1.firebaseio.com",
+   storageBucket: "coding-bootcamp-project1.appspot.com",
+   messagingSenderId: "639188769603"
+ };
+ firebase.initializeApp(config);
+
+ 
