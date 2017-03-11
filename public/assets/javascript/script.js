2d0fa4453d8aeadaae69c37e276ded7eef9f48be
rukaroa <rukaroa@gmail.com>
2017-03-11 12:10:35 -0600

    merged apiY.js

--- public/assets/javascript/script.js
+++ public/assets/javascript/script.js
@@ -21,6 +21,7 @@ messagingSenderId: "256422409939"
  	if (searchTerm == "") {} //do nothing if empty search
  	else {
 		queryRedditApi();
+		queryYouTubeAPI();
  	}
  });
 
@@ -35,6 +36,7 @@ messagingSenderId: "256422409939"
 		if (searchTerm == ""){} //do nothing
 		else {
 			queryRedditApi();
+			queryYouTubeAPI();
 		}
 	}
 });
@@ -91,5 +93,74 @@ function queryRedditApi() {
 		}
 	}).fail(function(err) {
 	  throw err;
+	});
+}
+
+function queryYouTubeAPI() {
+	var baseURL = "https://www.googleapis.com/youtube/v3/search?type=video&q="+searchTerm+"+gameplay&";
+
+	var params = {
+    	order: 'rating', 
+    	topic: 'video',
+    	chart: 'mostPopular',
+    	videoCategoryId: '20',
+    	key: 'AIzaSyAiFXSG9q5L_osKzM1JrzzoJnc7ouCsKYw',
+    	part: 'snippet'
+	};
+
+	var query = $.param(params);
+	var queryURL = (baseURL + query);
+
+	$.ajax({
+		url: queryURL,
+		method: "GET"
+	}).done(function(response){
+		//empty carousel on each search
+		$(".carousel-inner").empty();
+		$(".carousel-indicators").empty();
+
+		var idList = [];
+		//push videos to idList
+		for (var i=0;i<4;i++){
+			var videoId = response.items[i].id.videoId;
+			idList.push(videoId);
+		}
+
+		for (var i=0;i<idList.length;i++){
+			
+			var li = $("<li>");
+			//build carousel target lists
+			li.attr("data-target","#videos");
+			li.attr("data-slide-to", i);
+			//set index 0 to active
+			if (i == 0){
+				li.addClass("active");
+			}
+			
+			$(".carousel-indicators").append(li);
+
+			var div = $("<div>");
+			//build carousel items
+			div.addClass("item text-center");
+			//set index 0 to active
+			if (i == 0){
+				div.addClass("active");
+			}
+
+			var videoTag = $("<iframe>");
+			//build video iframe
+			videoTag.attr("width","80%");
+			videoTag.attr("height","270");
+			videoTag.attr("src","https://www.youtube.com/embed/"+idList[i]);
+			videoTag.attr("frameborder","0");
+			videoTag.attr("allowfullscreen","");
+			videoTag.attr("alt","Video-" + i);
+			
+			
+			div.append(videoTag);
+
+			$(".carousel-inner").append(div);
+
+		}	
 	});
 }
